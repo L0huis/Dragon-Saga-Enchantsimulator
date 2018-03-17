2bc68a915b2634cb5d7a166dbf57fd86f1bbaeba
Julian Lohuis <lohuisjulian@gmail.com>
2018-03-17 16:07:15 +0100

    .

--- Soulcraft_Enchant/Form1.Designer.cs
+++ Soulcraft_Enchant/Form1.Designer.cs
@@ -85,7 +85,7 @@
             this.tabControl.Margin = new System.Windows.Forms.Padding(0);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(466, 242);
+            this.tabControl.Size = new System.Drawing.Size(466, 262);
             this.tabControl.TabIndex = 0;
             // 
             // tabPage1
@@ -316,7 +316,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(463, 237);
+            this.tabPage2.Size = new System.Drawing.Size(458, 236);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Probabilities";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -538,7 +538,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 242);
+            this.ClientSize = new System.Drawing.Size(466, 262);
             this.Controls.Add(this.tabControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
